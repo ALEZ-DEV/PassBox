5dd39db1f2e0325d6d6f2d46ddc8b8f9dbb986c9
ALEZ <38623465+Raidersfocus@users.noreply.github.com>
2021-11-18 09:46:38 +0100

    Add files via upload

--- PassBox/main.py
+++ PassBox/main.py
@@ -5,172 +5,177 @@ import csv
 
 keepedKey = ""
 oneKeyKeeped = False
+fileName = ""
 
-def __keepKey__(key) :
-    
-    keepedKey = key
-    print("your key is keeped")
-
-    def __getKeepedKey__() :
-        return keepedKey
-    
-def __menu__() :
-
-    isChoosed = True
-    turn = 0
-    
-    while(isChoosed) :
-        
-        if (turn == 0) :
-                print("what do you want to do :")
-                print("n : create a new key (not recommanded if is already created)")
-                print("c : crypte anything file")
-                print("d : decrypte txt file")
-                print("np : create a new password list")
-                print("op : open password list")  
-                turn += 1     
-        
-        command = input("--")
-        if (command == "n") :
-            isChoosed = False
-            __newKey__()
-            __menu__()
-            
-        elif (command == "c") :
-            isChoosed = False
-            print("do you want to generate a new key ? | yes : y | no : n")
-            newOne = input("--")
-            
-            if (newOne == "n") :
-                print("do you want to use keeped key ? | yes : y | no : n")
-                getKey = input("--")           
-                if (getKey == "n") :
-                    print("Enter your key :")
-                    fileKey = input("--")
-                else :
-                    fileKey = keepedKey
-            else :
-                fileKey =  __newKey__()
-            
-            print("Enter the path of your file :")
-            path = input("--")
-            
-            __encrypteFile__(path, fileKey)
-            __menu__()
-            
-        elif (command == "d") :
-            isChoosed = False
-            
-            print("do you want to use keeped key ? | yes : y | no : n")
-            getKey = input("--")           
-            if (getKey == "n") :
-                print("Enter your key :")
-                fileKey = input("--")
-            else :
-                fileKey = keepedKey
-            
-            print("Enter the path of your file :")
-            path = input("--")
-            
-            __decryptFile__(path, fileKey)
-            
-        elif (command == "np") :
-            isChoosed = False
-            
-            print("do you want to use keeped key ? | yes : y | no : n")
-            getKey = input("--")           
-            if (getKey == "n") :
-                print("Enter your key :")
-                fileKey = input("--")
-            else :
-                fileKey = keepedKey
-            
-            print("Enter the name of your file :")
-            name = input("--")
-            
-            __createPassList__(fileKey, name)
-            __menu__()
-            
-        elif (command == "op") :
-            isChoosed = False
-            
-        else :
-            print("this command is not valid")
-            isChoosed = True
-            
+#def __menu__() :
+#
+#    isChoosed = True
+#    turn = 0
+#    
+#    while(isChoosed) :
+#        
+#        if (turn == 0) :
+#                print("what do you want to do :")
+#                print("n : create a new key (not recommanded if is already created)")
+#                print("c : crypte anything file")
+#                print("d : decrypte txt file")
+#                print("np : create a new password list")
+#                print("op : open password list")  
+#                turn += 1     
+#        
+#        command = input("--")
+#        if (command == "n") :
+#            isChoosed = False
+#            __newKey__()
+#            __menu__()
+#            
+#        elif (command == "c") :
+#            isChoosed = False
+#            print("do you want to generate a new key ? | yes : y | no : n")
+#            newOne = input("--")
+#            
+#            if (newOne == "n") :
+#                print("do you want to use keeped key ? | yes : y | no : n")
+#                getKey = input("--")           
+#                if (getKey == "n") :
+#                    print("Enter your key :")
+#                    fileKey = input("--")
+#                else :
+#                    fileKey = keepedKey
+#            else :
+#                fileKey =  __newKey__()
+#            
+#            print("Enter the path of your file :")
+#            path = input("--")
+#            
+#            __encrypteFile__(path, fileKey)
+#            __menu__()
+#            
+#        elif (command == "d") :
+#            isChoosed = False
+#            
+#            print("do you want to use keeped key ? | yes : y | no : n")
+#            getKey = input("--")           
+#            if (getKey == "n") :
+#                print("Enter your key :")
+#                fileKey = input("--")
+#            else :
+#                fileKey = keepedKey
+#            
+#            print("Enter the path of your file :")
+#            path = input("--")
+#            
+#            __decryptFile__(path, fileKey)
+#            
+#        elif (command == "np") :
+#            isChoosed = False
+#            
+#            print("do you want to use keeped key ? | yes : y | no : n")
+#            getKey = input("--")           
+#            if (getKey == "n") :
+#                print("Enter your key :")
+#                fileKey = input("--")
+#            else :
+#                fileKey = keepedKey
+#            
+#            print("Enter the name of your file :")
+#            name = input("--")
+#            
+#            __createPassList__(fileKey, name)
+#            __menu__()
+#            
+#        elif (command == "op") :
+#            isChoosed = False
+#            
+#        else :
+#            print("this command is not valid")
+#            isChoosed = True
+#            
 def __newKey__() :
     
     key = Fernet.generate_key()
-    print("the new key is : " + str(key))
-    
-    print("do you want to keep this key ? | yes : y | no : n")
-    keeped = input("--")
-
-    if (keeped == "y") :
-        __keepKey__(key)
+    print(key)
     
     return key
     
-def __encrypteFile__(data, encrypteKey) :
-    print("where do you want to save your encrypted file ?")
-    savePath = input("--")
+def __encrypteFile__(data, encrypteKey, path) :
     
     fKey = Fernet(bytes(encrypteKey, "utf-8"))
     
     readFile = open(data, "rb").read()
     cryptedFile = fKey.encrypt(readFile)
     
-    with open(savePath, "wb") as file :
+    with open(path, "wb") as file :
         file.write(cryptedFile)
 
-def __decryptFile__(data, decrypteKey) :
-    
-    print("where do you want to save your decrypted file ?")
-    savePath = input("--")
+def __decryptFile__(data, decrypteKey, path) :
     
     fKey = Fernet(bytes(decrypteKey, "utf-8"))
     
     readFile = open(data, "rb").read()
     decryptedFile = fKey.decrypt(readFile)
     
-    with open(savePath, "wb") as file :
+    with open(path, "wb") as file :
         file.write(decryptedFile)
         
-def __createPassList__(encrypteKey, nameFile) :
+def __createList__(name) :
+
+    open(f"%USERPROFILE%\\Documents\\PassBox\\{name}.pbl", "wb")
+    
+    return f"%USERPROFILE%\\Documents\\PassBox\\{name}.pbl"
+    
+def __addList__(path, key , name, data) :
+    
+    password = [name, data]
+    __encrypteList__(path, key, password)
+    __getList__(path, key)
+    
+def __getList__(path, decrypteKey):
+    
+    fKey = Fernet(bytes(decrypteKey, "utf-8"))
+    passList = []
+    
+    with open(path, "rb") as fileData : 
+        csv_reader = csv.reader(fileData)
+        
+    for i in csv_reader :
+        password = []
+        password.append(i[0])
+        password.append(i[1])
+        passList.append(password)
+        
+    return passList
+        
+def __encrypteList__(path, encrypteKey, passwordList) :
     
     fkey = Fernet(bytes(encrypteKey, "utf-8"))
-    isFinished = True
-    numLoop = 0
-    listOfPassword = []
-
-    while(isFinished) :
-        numLoop += 1
-        print(f"the name of the password {numLoop}")
-        name = input("--")
-        print("the password")
-        password = input("--")
+    data = []
+    
+    
+    with open(path, "rb") as fileData :
+        csv_reader = csv.reader(fileData)
         
-        fisrtList= [name, password]
-        listOfPassword.append(fisrtList)
-        
-        print("do you want to add more ? | yes : y | no : n")
-        add = input("--")
-        if(add == "n"):
-            isFinished = False
-        
-    cryptedFile = fkey.encrypt()
-    with open(f"%USERPROFILE%\douments\PassBox\{nameFile}.csv", "wb", newline="") as file :
-        csv_writer = csv.writer(file, delimiter=",")
-        
-        for i in range(len(listOfPassword)) :
+        for i in csv_reader :
+            defaultPassword = []
+            defaultPassword.append(i[0])
+            defaultPassword.append(i[1])
+            data.append(defaultPassword)  
             
-            data = listOfPassword[i]
-            csv_writer.writerow(data)
+    name = fkey.encrypt(passwordList[0])
+    password = fkey.encrypt(passwordList[1])
+    dataLine = []
+    dataLine.append(name)
+    dataLine.append(password)
+    data.append(dataLine)
     
-    print("the list is created")
+    
+    with open(path, "wb", newline="") as fileData :
+        csv_writer = csv.writer(fileData, delimiter=",")
+        
+        for i in data :
+            csv_writer.writerow(i)
 
-    
-__menu__()
 print("Programme finished")
 
+# the password file is in .pbl (.Pass Box List)
 #b'CZ51unnhvsGfVWvtP3Xg00Y-Attv-D-OsE7MjfKnegY='
