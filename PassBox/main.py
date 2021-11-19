d829e37dc64ea9dadb002e2e5853bab095d4ab17
ALEZ <38623465+Raidersfocus@users.noreply.github.com>
2021-11-19 18:25:47 +0100

    Add files via upload

--- PassBox/main.py
+++ PassBox/main.py
@@ -25,219 +25,253 @@ __encrypteList__ : encrypte list in .pbl file / path, encrypteKey (key), passwor
 keepedKey = ""
 oneKeyKeeped = False
 fileName = ""
-    
-def __newKey__() :
-    
+
+
+def __newKey__():
     key = Fernet.generate_key()
     print(key)
-    
+
     return key
-    
-def __encrypteFile__(filePath, encrypteKey, path) :
-    
+
+
+def __encrypteFile__(filePath, encrypteKey, path):
     fKey = Fernet(bytes(encrypteKey, "utf-8"))
-    
+
     readFile = open(filePath, "rb").read()
     cryptedFile = fKey.encrypt(readFile)
-    
-    with open(path, "wb") as file :
+
+    with open(path, "wb") as file:
         file.write(bytes(cryptedFile))
 
-def __decryptFile__(data, decrypteKey, path) :
-    
+
+def __decryptFile__(data, decrypteKey, path):
     fKey = Fernet(bytes(decrypteKey, "utf-8"))
-    
+
     readFile = open(data, "rb").read()
     decryptedFile = fKey.decrypt(readFile)
-    
-    with open(path, "wb") as file :
+
+    with open(path, "wb") as file:
         file.write(decryptedFile)
 
-def __replaceBytes__(data) :
-    data.replace("b","")
-    data.replace("'","")
-    return data
-        
-def __createList__(name) :
 
-    open(f"%USERPROFILE%\\Documents\\PassBox\\{name}.pbl", "wb")
-    
-    return f"%USERPROFILE%\\Documents\\PassBox\\{name}.pbl"
-    
-def __addList__(path, key , name, data) :
-    
+def __replaceBytes__(data):
+    data.replace("b", "")
+    data.replace("'", "")
+    return data
+
+
+def __createList__(name):
+    open(f"%USERPROFILE%\\Documents\\{name}.pbl", "wb")
+
+    return f"%USERPROFILE%\\Documents\\{name}.pbl"
+
+
+def __addList__(path, key, name, data):
     password = [name, data]
     __encrypteList__(path, key, password)
     __getList__(path, key)
-    
+
+
 def __getList__(path, decrypteKey):
-    
     fKey = Fernet(bytes(decrypteKey, "utf-8"))
     passList = []
-    
-    with open(path, "rb") as fileData : 
+
+    with open("C:\\Users\\cp-21sgr\\Documents\\text.csv", "rb") as fileData:
         csv_reader = csv.reader(fileData)
-        
-    for i in csv_reader :
+
+    for i in csv_reader:
         password = []
-        password.append(i[0])
-        password.append(i[1])
+        password.append(fKey.decrypt(i[0]))
+        password.append(fKey.decrypt(i[1]))
         passList.append(password)
-        
+
     return passList
-        
-def __encrypteList__(path, encrypteKey, passwordList) :
-    
+
+
+def __encrypteList__(path, encrypteKey, passwordList):
     fkey = Fernet(bytes(encrypteKey, "utf-8"))
     data = []
-    
-    
-    with open(path, "rb") as fileData :
+
+    with open(path, "rb") as fileData:
         csv_reader = csv.reader(fileData)
-        
-        for i in csv_reader :
+
+        for i in csv_reader:
             defaultPassword = []
             defaultPassword.append(i[0])
             defaultPassword.append(i[1])
-            data.append(defaultPassword)  
-            
+            data.append(defaultPassword)
+
     name = fkey.encrypt(passwordList[0])
     password = fkey.encrypt(passwordList[1])
     dataLine = []
     dataLine.append(name)
     dataLine.append(password)
     data.append(dataLine)
-    
-    
-    with open(path, "wb", newline="") as fileData :
+
+    with open(path, "wb", newline="") as fileData:
         csv_writer = csv.writer(fileData, delimiter=",")
-        
-        for i in data :
+
+        for i in data:
             csv_writer.writerow(i)
 
 
-
-def __crypte__() :
-    
-    def __getPath__() :
+def __crypte__():
+    def __getPath__():
         path = askopenfile()
         entry_path.delete(0, END)
         entry_path.insert(0, path.name)
-        
-    def __generateKey__() :
+
+    def __generateKey__():
         key = __newKey__()
         entry_key.delete(0, END)
         entry_key.insert(0, key)
-        
-    def __crypte__() :
+
+    def __crypte__():
         filePath = entry_path.get()
         key = entry_key.get()
         path = userDocuments + "text.txt"
         __encrypteFile__(filePath, key, path)
-    
+
     crypte = Tk()
-    
+
     crypte.geometry("500x300")
-    
+
     crypte.title("PassBox : Crypte")
-    
+
     text_path = Label(crypte, text="enter the path", width=10)
-    entry_path = Entry(crypte,width=50)
+    entry_path = Entry(crypte, width=50)
     button_getFilePath = Button(crypte, text="...", width=15, command=__getPath__)
-    
+
     text_path.grid(column=0, row=1)
     entry_path.grid(column=1, row=1)
     button_getFilePath.grid(column=2, row=1)
-    
+
     text_key = Label(crypte, text="enter key", width=10)
     entry_key = Entry(crypte, width=50)
     button_generateKey = Button(crypte, text="generate", width=15, command=__generateKey__)
-    
+
     text_key.grid(column=0, row=2)
     entry_key.grid(column=1, row=2)
     button_generateKey.grid(column=2, row=2)
-    
+
     button_crypte = Button(crypte, text="crypte", width=10, command=__crypte__)
-    
+
     button_crypte.grid(column=1, row=3)
-    
+
     crypte.mainloop()
 
-def __decrypte__() :
-    
-    def __getPath__() :
+
+def __decrypte__():
+    def __getPath__():
         path = askopenfile()
         entry_path.delete(0, END)
         entry_path.insert(0, path.name)
-        
-    def __generateKey__() :
-        key = __newKey__()
-        entry_key.delete(0, END)
-        entry_key.insert(0, key)
-        
-    def __decrypte__() :
+
+    def __decrypte__():
         filePath = entry_path.get()
         key = entry_key.get()
         path = userDocuments + "text.txt"
         __decryptFile__(filePath, key, path)
-    
+
     decrypte = Tk()
-    
+
     decrypte.geometry("500x300")
-    
+
     decrypte.title("PassBox : Decrypte")
-    
+
     text_path = Label(decrypte, text="enter the path", width=10)
-    entry_path = Entry(decrypte,width=50)
+    entry_path = Entry(decrypte, width=50)
     button_getFilePath = Button(decrypte, text="...", width=15, command=__getPath__)
+
+    text_path.grid(column=0, row=1)
+    entry_path.grid(column=1, row=1)
+    button_getFilePath.grid(column=2, row=1)
+
+    text_key = Label(decrypte, text="enter key", width=10)
+    entry_key = Entry(decrypte, width=50)
+
+    text_key.grid(column=0, row=2)
+    entry_key.grid(column=1, row=2)
+
+    button_decrypte = Button(decrypte, text="decrypte", width=10, command=__decrypte__)
+
+    button_decrypte.grid(column=1, row=3)
+
+    decrypte.mainloop()
+
+
+def __passList__(list):
+
     
+    passList = Tk()
+
+    passList.geometry("500x300")
+
+    passList.title("PassBox : PassList")
+
+    def __getPath__():
+        path = askopenfile()
+        entry_path.delete(0, END)
+        entry_path.insert(0, path.name)
+
+    def __get__():
+        list = __getList__(entry_path.get(), entry_key.get())
+        n = 2
+        for i in list :
+            n += 1
+            name = Label(text=str(i[0]))
+            password = Label(text=str(i[1]))
+
+            name.grid(column=0, row=n)
+            password.grid(column=1, row=n)
+
+    text_path = Label(passList, text="enter the path", width=10)
+    entry_path = Entry(passList, width=50)
+    button_getFilePath = Button(passList, text="...", width=15, command=__getPath__)
+
     text_path.grid(column=0, row=1)
     entry_path.grid(column=1, row=1)
     button_getFilePath.grid(column=2, row=1)
     
-    text_key = Label(decrypte, text="enter key", width=10)
-    entry_key = Entry(decrypte, width=50)
-    button_generateKey = Button(decrypte, text="generate", width=15, command=__generateKey__)
-    
+    text_key = Label(passList, text="enter key", width=10)
+    entry_key = Entry(passList, width=50)
+
     text_key.grid(column=0, row=2)
     entry_key.grid(column=1, row=2)
-    button_generateKey.grid(column=2, row=2)
     
-    button_decrypte = Button(decrypte, text="decrypte", width=10, command=__decrypte__)
-    
-    button_decrypte.grid(column=1, row=3)
-    
-    decrypte.mainloop()
+    button_get = Button(passList, text="get", width=15, command=__get__)
 
-def __passList__() :
-    passList = Tk()
-    
-    passList.geometry("500x300")
-    
-    passList.title("PassBox : PassList")
+    button_get.grid(column=2, row=2)
 
-    
-def __exit__() :
+    passList.mainloop()
+
+def __test__() :
+    __passList__(None)
+
+def __exit__():
     exit()
 
-def __init__() :
-    
+
+def __init__():
+
     menu = Tk()
-    
+
     menu.geometry("500x300")
-    
+
     menu.title("PassBox")
-    
+
     button_crypte = Button(menu, text="crypte file", command=__crypte__, width=70)
     button_decrypte = Button(menu, text="decrypte file", command=__decrypte__, width=70)
-    button_passList = Button(menu, text="password list", command=__passList__)
+    button_passList = Button(menu, text="password list", command=__test__, width=70)
     button_exit = Button(menu, text="Exit", command=menu.destroy, width=70)
-    
+
     button_crypte.grid(column=0, row=1)
     button_decrypte.grid(column=0, row=2)
-    button_exit.grid(column=0, row=3)
-    
+    button_passList.grid(column=0, row=3)
+    button_exit.grid(column=0, row=4)
+
     menu.mainloop()
-    
+
+
 __init__()
-#IbwP6SKQJPMAgF0922lh41NL5gnMQymB-HWnidMincM=
+# IbwP6SKQJPMAgF0922lh41NL5gnMQymB-HWnidMincM=
